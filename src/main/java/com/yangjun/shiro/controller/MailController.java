3caafd1e1ac1dfb4c8cb5dedadec7e021281b43f
echoRookie <echorookie@gmail.com>
2021-04-04 16:36:53 +0800

    feat：测试发送带附件的邮件

--- src/main/java/com/yangjun/shiro/controller/MailController.java
+++ src/main/java/com/yangjun/shiro/controller/MailController.java
@@ -3,8 +3,14 @@ package com.yangjun.shiro.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @author yangjun6
@@ -22,6 +28,23 @@ public class MailController {
         mailMessage.setFrom("1171349468@qq.com");
         mailMessage.setText("这是测试内容");
         javaMailSender.send(mailMessage);
+        System.out.println("发送成功");
+    }
+    @GetMapping("/sendMimeMail")
+    public void sendMime(){
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setSubject("测试");
+            helper.setTo("1171349468@qq.com");
+            helper.setFrom("1171349468@qq.com");
+            helper.setText("这是测试内容");
+            helper.addAttachment("酒店发票1.jpg", new File("D:\\MyData\\yangjun6.CN\\Desktop\\工作\\12月份差旅报销\\酒店发票1.jpg"));
+            javaMailSender.send(message);
+            System.out.println("发送成功");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
         System.out.println("发送成功");
     }
 }
